d944e1d929357930271932c460b22755d031c2b6
JakovTomasic <jakov.tomasic1@gmail.com>
2023-02-02 19:33:48 +0100

    keypad added

--- AVR-alarm-system/main.c
+++ AVR-alarm-system/main.c
@@ -1,18 +1,67 @@
+
+# define F_CPU 7372800UL
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 #include "lcd.h"
 
-int main(void) {
-	
-	DDRD = _BV(4);
+#define KEYPAD_PORT PORTA
+#define KEYPAD_DDR 	DDRA
+#define KEYPAD_PIN 	PINA
 
-	TCCR1A = _BV(COM1B1) | _BV(WGM10);
-	TCCR1B = _BV(WGM12) | _BV(CS11);
+void initLcd() {
+	
+	DDRD |= _BV(4);
+
+	TCCR1A |= _BV(COM1B1) | _BV(WGM10);
+	TCCR1B |= _BV(WGM12) | _BV(CS11);
 	OCR1B = 128;
 
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
 	lcd_puts("Hello World");
+}
 
-	while (1);
+uint8_t getKeyPressed()
+{
+	uint8_t r,r2,c;
+
+	// Activate pull-up resistor for row pins.
+	// Do not set col pins (assigning zeros would set it)
+	KEYPAD_PORT |= 0X0F;
+
+	for(c = 0; c < 3 ; c++)
+	{
+		// Set all pins as input (pin 7 is not used so ignore it)
+		KEYPAD_DDR &= 0X80;
+
+		// Set only current col pin as output.
+		// PORT is always 0 for that pins so the output will be low (sink)
+		KEYPAD_DDR |= (0X40 >> c);
+		for(r = 0; r < 4; r++)
+		{
+			r2 = 3 - r;
+			if(!(KEYPAD_PIN & (0X08>>r2)))
+			{
+				return (r2*3+c);
+			}
+		}
+	}
+
+	//Indicate No keypressed
+	return 0XFF;
+}
+
+
+int main(void) {
+	
+	initLcd();
+	
+	while (1) {
+		_delay_ms(500);
+		
+		lcd_clrscr();
+		lcd_putc('0' + (getKeyPressed() % 10));
+	}
 }
