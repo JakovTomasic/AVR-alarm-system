5319ef82e2308f8ebdd6b97b825e9a03109af04e
JakovTomasic <jakov.tomasic1@gmail.com>
2023-02-03 15:01:35 +0100

    motion detector fix - pin change, keypad pins moved and created functions for easier changing them

--- AVR-alarm-system/main.c
+++ AVR-alarm-system/main.c
@@ -11,11 +11,12 @@
 #define KEYPAD_DDR 	DDRA
 #define KEYPAD_PIN 	PINA
 
-#define MOTION_PIN 			PIND
+#define MOTION_PIN 			PINA
+#define MOTION_PORT			PORTA
 #define MOTION_PIN_NUMBER	0
 
-#define BUZZER_DDR 			DDRA
-#define BUZZER_PORT			PORTA
+#define BUZZER_DDR 			DDRC
+#define BUZZER_PORT			PORTC
 #define BUZZER_PIN_NUMBER	7
 
 void initLcd() {
@@ -39,6 +40,7 @@ void initLcd() {
 }
 
 uint8_t readMotion() {
+	//MOTION_PORT |= _BV(MOTION_PIN_NUMBER); // TODO: pull-up
 	return bit_is_set(MOTION_PIN, MOTION_PIN_NUMBER);
 }
 
@@ -49,34 +51,51 @@ void buzz() {
 	BUZZER_PORT &= ~(_BV(BUZZER_PIN_NUMBER));
 }
 
+void keypad_activateRowPullUps() {
+	// Do not set col pins (assigning 0 would set it)
+	KEYPAD_PORT |= 0X1E;
+}
+
+void keypad_setAllInput() {
+	// One pin is not used so ignore it
+	KEYPAD_DDR &= 0X01;
+}
+
+void keypad_setColAsOutputLow(uint8_t col) {
+	// PORTX is always 0 for that pins so the output will be low (sink)
+	KEYPAD_DDR |= (0X80 >> col);
+}
+
+uint8_t keypad_isRowLow(uint8_t row) {
+	return !(KEYPAD_PIN & (0X10>>row));
+}
+
 uint8_t getKeyPressed()
 {
 	uint8_t r, c, reversedC;
 
-	// Activate pull-up resistor for row pins.
-	// Do not set col pins (assigning zeros would set it)
-	KEYPAD_PORT |= 0X0F;
+	keypad_activateRowPullUps();
 
 	for(reversedC = 0; reversedC < 3 ; reversedC++)
 	{
 		c = 2 - reversedC;
 		
-		// Set all pins as input (pin 7 is not used so ignore it)
-		KEYPAD_DDR &= 0X80;
+		keypad_setAllInput();
 
 		// Set only current col pin as output.
-		// PORT is always 0 for that pins so the output will be low (sink)
-		KEYPAD_DDR |= (0X40 >> c);
+		keypad_setColAsOutputLow(c);
 		for(r = 0; r < 4; r++)
 		{
-			if(!(KEYPAD_PIN & (0X08>>r)))
+			if(keypad_isRowLow(r))
 			{
+				// Return pressed key identifier.
+				// Do not check if other keys are pressed.
 				return (r*3+c)+1;
 			}
 		}
 	}
 
-	//Indicate No keypressed
+	// Indicate no key pressed
 	return 0;
 }
 
@@ -87,18 +106,18 @@ int main(void) {
 
 	buzz();
 
-	uint8_t lastKey = 0;
-
+	uint8_t key, lastKey = 0;
+	
 	while (1) {
 		_delay_ms(50);
 		
 		lcd_clrscr();
 
-		uint8_t key = getKeyPressed();
+		key = getKeyPressed();
 
 		lcd_putc('0' + (key % 10));
 		lcd_gotoxy(0, 1);
-		lcd_putc('0' + (readMotion() % 10));
+		lcd_putc('0' + readMotion());
 		
 		if (key && key != lastKey) {
 			buzz();
