f8f75b04ff58d1a3086bfd182314ad9ec60ac861
JakovTomasic <jakov.tomasic1@gmail.com>
2023-02-08 02:21:45 +0100

    SD card troubles

--- AVR-alarm-system/main.c
+++ AVR-alarm-system/main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h> // TODO: treba li mi ovo?
 
 #include "lcd.h"
+//#include "fat.h"
 
 #define KEYPAD_PORT			PORTA
 #define KEYPAD_DDR 			DDRA
@@ -275,10 +276,59 @@ void checkMotion() {
 }
 
 
-
+/*
 int main(void) {
 	
+	
 	initLcd();
+	lcd_clrscr();
+	
+	DIR dir;
+	FILE file;
+	uint8_t return_code = 0;
+	uint8_t dirItems = 0;
+	uint8_t* read_buf = 0;
+	
+	lcd_puts("my start");
+	
+	// Mount the memory card
+	return_code = FAT_mountVolume();
+	
+	lcd_putc('0' + (return_code % 10));
+	
+	// If no error
+	if(return_code == MR_OK){
+		lcd_puts("ok");
+		// Open folder
+		return_code = FAT_openDir(&dir, "/Logging");
+		if(return_code == FR_OK){
+			// ... optionally print folder name using an LCD library: print(FAT_getFilename());
+			
+			// Get number of folders and files inside the directory
+			dirItems = FAT_dirCountItems(&dir);
+			
+			// Open a file for reading or writing
+			return_code = FAT_fopen(&dir, &file, "log.txt");
+			
+			// Read block of 512 bytes until the end-of-file flag is set
+			while(FAT_feof(&file) == 0 && FAT_ferror(&file) == 0){
+				read_buf = FAT_fread(&file);
+				
+				// Function included in DIS_ST7735.h LCD library
+				DIS_ST7735_drawString((char*)read_buf);
+			}
+			
+			// Move file read pointer back to the beginning (optional)
+			FAT_fseek(&file, 0);
+		}
+		
+	}
+	
+	
+	while(1);
+	*/
+	
+	/*
 	
 	state = DEFAULT_STATE;
 	refreshState();
@@ -303,14 +353,14 @@ int main(void) {
 		checkMotion();
 
 		key = getKeyPressed();
-
+*/
 		/*
 		lcd_clrscr();
 		lcd_putc('0' + (key % 10));
 		lcd_gotoxy(0, 1);
 		lcd_putc('0' + readMotion());
 		*/
-		
+		/*
 		if (key != KEY_NONE && key != lastKey) {
 			handleKeypress(key);
 			buzz();
@@ -318,8 +368,10 @@ int main(void) {
 		
 		lastKey = key;
 	}
-}
+	*/
+//}
 
+/*
 // TODO: do i need this timer? Maybe start it only on motion detect and stop it if correct password entered
 ISR(TIMER1_COMPA_vect)
 {
@@ -334,4 +386,172 @@ ISR(TIMER1_COMPA_vect)
 			refreshState();
 		}
 	}
+}
+*/
+
+
+
+//Adding all the codes:
+#include <avr/iom16.h>
+#define FOSC 7372800
+char chars[512];
+
+
+#define DI 4                         // Port B bit 6 (pin7): data in (data from MMC)
+#define DT 5                        // Port B bit 5 (pin6): data out (data to MMC)
+#define CLK 1                     // Port B bit 7 (pin8): clock
+#define CS 6                        // Port B bit 4 (pin5): chip select for MMC
+//SPI Initialization:
+void ini_SPI() {
+	DDRB &= ~(_BV(DI));                     //input
+	DDRB |= _BV(CLK);                     //outputs
+	DDRB |= _BV(DT);                     //outputs
+	DDRB |= _BV(CS);                     //outputs
+	SPCR |= _BV(SPE);                     //SPI enable
+	SPCR |= _BV(MSTR);                     //Master SPI mode
+	SPCR &= ~(_BV(SPR1));                    //fosc/16
+	SPCR |= _BV(SPR0);                    //fosc/16
+	SPSR &= ~(_BV(SPI2X));                    //speed is not doubled
+	PORTB &= ~(_BV(CS));                     //Enable CS pin for the SD card
+}
+//Functions for sending and receiving one byte through SPI:
+char SPI_sendchar(char chr) {
+	char receivedchar = 0;
+	SPDR = chr;
+	while(!(SPSR & (1<<SPIF)));
+	receivedchar = SPDR;
+	return (receivedchar);
+}
+//Function to send a command frame Command:
+char Command(char cmd, uint16_t ArgH, uint16_t ArgL, char crc ) {
+	SPI_sendchar(0xFF);
+	SPI_sendchar(cmd);
+	SPI_sendchar((uint8_t)(ArgH >> 8));
+	SPI_sendchar((uint8_t)ArgH);
+	SPI_sendchar((uint8_t)(ArgL >> 8));
+	SPI_sendchar((uint8_t)ArgL);
+	SPI_sendchar(crc);
+	SPI_sendchar(0xFF);
+	return SPI_sendchar(0xFF);                // Returns the last byte received
+}
+uint8_t toBin(uint8_t aa) {
+	if (aa) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+//Initialization card:
+void ini_SD(void) {
+	char i;
+	PORTB |= _BV(CS);                    //disable CS
+	for(i=0; i < 10; i++)
+		SPI_sendchar(0xFF);                // Send 10 * 8 = 80 clock pulses 400 kHz
+	PORTB &= ~(_BV(CS));                 //enable CS
+	for(i=0; i < 2; i++)
+		SPI_sendchar(0xFF);                // Send 2 * 8 = 16 clock pulses 400 kHz
+	Command(0x40,0,0,0x95);              // reset
+	lcd_puts("g");
+	idle_no:
+	lcd_clrscr();
+	lcd_puts("g");
+	char temp = Command(0x41,0,0,0xFF);
+	lcd_puts("T");
+	lcd_putc('0' + toBin(temp & _BV(7)));
+	lcd_putc('0' + toBin(temp & _BV(6)));
+	lcd_putc('0' + toBin(temp & _BV(5)));
+	lcd_putc('0' + toBin(temp & _BV(4)));
+	lcd_putc('0' + toBin(temp & _BV(3)));
+	lcd_putc('0' + toBin(temp & _BV(2)));
+	lcd_putc('0' + toBin(temp & _BV(1)));
+	lcd_putc('0' + toBin(temp & _BV(0)));
+	if (temp !=0)
+	//goto idle_no;                        //idle = L?
+	lcd_puts("T");
+	SPCR &= ~(_BV(SPR0));                //fosc/4
+}
+//Writing to the card:
+//The function returns 1 if an error occurs  else returns 0 if successful
+int write(void) {
+	int i;
+	uint8_t wbr;
+	//Set write mode 512 bytes
+	if (Command(0x58,0,512,0xFF) !=0) {
+		//Determine value of the response byte 0 = no errors
+		return 1;
+		//return value 1 = error
+	}
+	SPI_sendchar(0xFF);
+	SPI_sendchar(0xFF);
+	SPI_sendchar(0xFE);
+	//recommended by posting a terminator sequence [2]
+	//write data from chars [512] tab
+	uint16_t ix;
+	char r1 =  Command(0x58,0,512,0xFF);
+	for (ix = 0; ix < 50000; ix++) {
+		if (r1 == (char)0x00) break;
+		r1 = SPI_sendchar(0xFF);
+	}
+	if (r1 != (char)0x00) {
+		return 1;
+		//return value 1 = error
+	}
+	//recommended by the control loop [2]
+	SPI_sendchar(0xFF);
+	SPI_sendchar(0xFF);
+	wbr = SPI_sendchar(0xFF);
+	//write block response and testing error
+	wbr &= 0x1F;
+	//zeroing top three indeterminate bits 0b.0001.1111
+	if (wbr != 0x05) { // 0x05 = 0b.0000.0101
+		//write error or CRC error
+		return 1;
+	}
+	while(SPI_sendchar(0xFF) != (char)0xFF);
+	//wait for the completion of a write operation to the card
+	return 0;
+}
+//Reading from the card:
+//The function returns 1 if an error occurs  else returns 0 if successful
+int read(void) {
+	int i;
+	uint16_t ix;
+	char r1 =  Command(0x51,0,512,0xFF);
+	for (ix = 0; ix < 50000; ix++) {
+		if (r1 == (char)0x00) break;
+		r1 = SPI_sendchar(0xFF);
+	}
+	if (r1 != (char)0x00) {
+		return 1;
+	}
+	//read from the card will start after the framework
+	while(SPI_sendchar(0xFF) != (char)0xFE);
+	for(i=0; i < 512; i++) {
+		while(!(SPSR & (1<<SPIF)));
+		chars[i] = SPDR;
+		SPDR = SPI_sendchar(0xFF);
+	}
+	SPI_sendchar(0xFF);
+	SPI_sendchar(0xFF);
+	return 0;
+}
+int main(void) {
+	initLcd();
+	lcd_puts("1");
+	ini_SPI();
+	lcd_puts("2");
+	ini_SD();
+	lcd_puts("3");
+	sei();
+	lcd_puts("4");
+	write();
+	lcd_puts("5");
+	read();
+	lcd_puts("6");
+	
+	lcd_puts("jeej");
+	
+	while(1);
+	
+	return 0;
 }
