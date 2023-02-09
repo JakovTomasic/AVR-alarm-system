800d1d7e58d7ed508a9f6b0ec62ad19276e8416a
JakovTomasic <jakov.tomasic1@gmail.com>
2023-02-09 21:42:45 +0100

    keypad logic optimised little bit

--- AVR-alarm-system/keypad.c
+++ AVR-alarm-system/keypad.c
@@ -4,46 +4,29 @@
 #include "keypad.h"
 
 
-// TODO: make faster using define
-void keypad_activateRowPullUps() {
-	// Do not set col pins (assigning 0 would set it)
-	KEYPAD_PORT |= 0X1E;
-}
-
-// TODO: make faster using define
-void keypad_setAllInput() {
-	// One pin is not used so ignore it
-	KEYPAD_DDR &= 0X01;
-}
-
-// TODO: make faster using define
-void keypad_setColAsOutputLow(uint8_t col) {
-	// PORTX is always 0 for that pins so the output will be low (sink)
-	KEYPAD_DDR |= (0X80 >> col);
-}
-
-// TODO: make faster using define
-uint8_t keypad_isRowLow(uint8_t row) {
-	return bit_is_clear(KEYPAD_PIN, 4 - row);
-}
-
 uint8_t getKeyPressed(void)
 {
 	uint8_t r, c, reversedC;
 
-	keypad_activateRowPullUps();
+	// Activate pull-up resistors for row pins.
+	// Do not set col pins (assigning 0 would set it).
+	KEYPAD_PORT |= 0X1E;
 
 	for(reversedC = 0; reversedC < 3 ; reversedC++)
 	{
 		c = 2 - reversedC;
 		
-		keypad_setAllInput();
+		// Set all pins as input.
+		// One pin is not used so ignore it
+		KEYPAD_DDR &= 0X01;
 
-		// Set only current col pin as output.
-		keypad_setColAsOutputLow(c);
+		// Set only current col pin as output. Write logical 0 to it.
+		// PORTX is always 0 for that pins so the output will be low (sink)
+		KEYPAD_DDR |= (0X80 >> c);
+		
 		for(r = 0; r < 4; r++)
 		{
-			if(keypad_isRowLow(r))
+			if(bit_is_clear(KEYPAD_PIN, 4 - r))
 			{
 				// Return pressed key.
 				// Do not check if other keys are pressed.
@@ -63,5 +46,3 @@ uint8_t getKeyPressed(void)
 	// Indicate no key pressed
 	return KEY_NONE;
 }
-
-
