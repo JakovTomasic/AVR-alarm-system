7ddaf53e5814e91e4fc85f01e14751f003c22b47
JakovTomasic <jakov.tomasic1@gmail.com>
2023-02-10 00:08:40 +0100

    door servo pwm values calcualted exactly

--- AVR-alarm-system/door.c
+++ AVR-alarm-system/door.c
@@ -5,9 +5,9 @@
 
 void initDoor(void) {
 	
-	DDRD |= (1<<PD5);	/* Make OC1A pin as output */
-	TCNT1 = 0;			/* Set timer1 count zero */
-	ICR1 = 2499;		/* Set TOP count for timer1 in ICR1 register */
+	DDRD |= (1<<PD5);			/* Make OC1A pin as output */
+	TCNT1 = 0;					/* Set timer1 count zero */
+	ICR1 = SERVO_PWM_TOP;		/* Set TOP count for timer1 in ICR1 register */
 	OCR1A = SERVO_OPEN_OCR;
 
 	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
--- AVR-alarm-system/door.h
+++ AVR-alarm-system/door.h
@@ -2,9 +2,13 @@
 #define DOOR_H_
 
 
-// TODO: izracunaj prave vrijednosti i postavi
-#define SERVO_OPEN_OCR 175
-#define SERVO_CLOSED_OCR 300
+// TOP = f_CPU / (F * N) - 1
+// TOP = 7372800UL / (50 * 64) - 1
+// -90 degrees is approximately 0.5ms duty cycle -> OCR = 57
+//	90 degrees is approximately 2.4ms duty cycle -> OCR = 277
+#define SERVO_PWM_TOP 2303
+#define SERVO_OPEN_OCR 277
+#define SERVO_CLOSED_OCR 57
 
 extern void initDoor(void);
 
